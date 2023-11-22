8d510335857b834c4c7a866b7ded6d78ef464a9c
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-11-22 17:57:12 +0100

    3

--- strategy/src/main/java/org/example/Main.java
+++ strategy/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -7,10 +9,18 @@ public class Main {
 
         Context context =new Context();
         Scanner scanner = new Scanner(System.in);
+        Map<String, Strategy> strategyMap=new HashMap<>();
+        Strategy strategy;
         while (true){
-            System.out.println("Quelle stratégie \n");
-            String strategyClassName = scanner.nextLine();
-            Strategy strategy = (Strategy) Class.forName(strategyClassName).newInstance();
+            System.out.println("Quelle stratégie");
+            String str = scanner.nextLine();
+            strategy=strategyMap.get(str);
+            if(strategy==null){
+                System.out.println("Creation nv obj de StrategyImpl"+str);
+                 strategy = (Strategy) Class.forName("org.example.StrategyImpl"+str).getConstructor().newInstance();
+                 strategyMap.put(str,strategy);
+            }
+
             context.setStrategy(strategy);
             context.effectuerOperation();
 
