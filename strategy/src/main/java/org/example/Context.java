38473067166358e13767bdd2e50c2634abe75573
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-11-22 16:07:57 +0100

    1

---
+++ strategy/src/main/java/org/example/Context.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Context {
+    public void effectuerOperation(){
+        System.out.println("****************");
+        System.out.println("------------------");
+        System.out.println("##############");
+    }
+}
