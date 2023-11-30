bdddb5899a842c625ebaf9fca55ca55dc530b568
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-11-30 20:30:09 +0100

    addig readme file

--- composite/src/main/java/org/example/Main.java
+++ composite/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.compo.Component;
+import org.example.compo.Folder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Folder root=new Folder("Design Patterns");
+        Folder d1=(Folder) root.addComponent(new Folder("Component"));
+        Folder d2=(Folder)root.addComponent(new Folder("Structure"));
+        Folder d3=(Folder)root.addComponent(new Folder("Création"));
+        root.show();
+
+
     }
 }
