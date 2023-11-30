5aaef2af7c2600a7762d04fcbc141b7fc54e4b9a
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-11-30 20:52:27 +0100

    last one

--- composite/README.md
+++ composite/README.md
@@ -1,5 +1,17 @@
 # Pattern Composite - ZIGHIGHI ZAKARIA - GLSID 3
-------> TEST 1 : 
+**------> TEST 1 :** 
 ![image](https://github.com/zighighizakaria/DesignPatternsTps2024/assets/99823896/f66b5c60-ce57-4358-a7b2-8992d9391df7)
+
 ----------------
+
+**------> TEST 2 :**
+![img.png](img.png)
+
+-------------
+**------> TEST 3 :**
+
+![img_1.png](img_1.png)
+
+--------------
+
 
--- composite/src/main/java/org/example/Main.java
+++ composite/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.compo.Component;
+import org.example.compo.File;
 import org.example.compo.Folder;
 
 public class Main {
@@ -10,6 +11,20 @@ public class Main {
         Folder d1=(Folder) root.addComponent(new Folder("Component"));
         Folder d2=(Folder)root.addComponent(new Folder("Structure"));
         Folder d3=(Folder)root.addComponent(new Folder("Création"));
+        d1.addComponent(new File("Strategy"));
+        d1.addComponent(new File("Observer"));
+        d2.addComponent(new File("Decorator"));
+        d2.addComponent(new File("Composite"));
+        d2.addComponent(new File("Adapter"));
+        d3.addComponent(new File("Singleton"));
+        d3.addComponent(new File("Builder"));
+        Folder d21 = (Folder) d2.addComponent(new Folder("Str21"));
+        d21.addComponent(new File("C211"));
+        d21.addComponent(new File("C212"));
+        d21.addComponent(new File("C213"));
+
+
+
         root.show();
 
 
